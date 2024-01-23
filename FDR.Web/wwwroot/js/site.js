9e716662fddfe40451b323f7097417ee9219a51a
FDR <fdr@fdr.hu>
2024-01-23 08:43:47 +0100

    JS refactor

--- FDR.Web/wwwroot/js/site.js
+++ FDR.Web/wwwroot/js/site.js
@@ -2,3 +2,13 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function SelectDrive() {
+    $('#SelectedFolder').val($('#Drives').val());
+    $('#SubfoldersDiv').load(`/Folder/Subfolders?folder=${encodeURIComponent($('#SelectedFolder').val())}`);
+}
+
+function SelectSubfolder() {
+    $('#SelectedFolder').val($('#Subfolders').val());
+    $('#SubfoldersDiv').load(`/Folder/Subfolders?folder=${encodeURIComponent($('#SelectedFolder').val())}`);
+}
