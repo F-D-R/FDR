9142c599a41dd732ec868d383249bb27aeee2d94
FDR <fdr@fdr.hu>
2024-01-23 09:17:43 +0100

    FolderPopup and JS changes

--- FDR.Web/wwwroot/js/site.js
+++ FDR.Web/wwwroot/js/site.js
@@ -1,14 +1,12 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
-
 function SelectDrive() {
     $('#SelectedFolder').val($('#Drives').val());
-    $('#SubfoldersDiv').load(`/Folder/Subfolders?folder=${encodeURIComponent($('#SelectedFolder').val())}`);
+    $('#Subfolders').load(`/Folder/Subfolders?folder=${encodeURIComponent($('#SelectedFolder').val())}`);
 }
 
 function SelectSubfolder() {
     $('#SelectedFolder').val($('#Subfolders').val());
-    $('#SubfoldersDiv').load(`/Folder/Subfolders?folder=${encodeURIComponent($('#SelectedFolder').val())}`);
+    $('#Subfolders').load(`/Folder/Subfolders?folder=${encodeURIComponent($('#SelectedFolder').val())}`);
 }
