47a1048db5efd8e711bd6e052cac235de339f352
FDR <fdr@fdr.hu>
2024-02-01 14:56:27 +0100

    Unifying the folder popup partials with the scripts

--- FDR.Web/wwwroot/js/site.js
+++ FDR.Web/wwwroot/js/site.js
@@ -1,12 +1,3 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-function SelectDrive() {
-    $('#SelectedFolder').val($('#Drives').val());
-    $('#Subfolders').load(`/Folder/Subfolders?folder=${encodeURIComponent($('#SelectedFolder').val())}`);
-}
-
-function SelectSubfolder() {
-    $('#SelectedFolder').val($('#Subfolders').val());
-    $('#Subfolders').load(`/Folder/Subfolders?folder=${encodeURIComponent($('#SelectedFolder').val())}`);
-}
