c4ee657e2e1de1b3a6790ec8d5218f33d16c2249
FDR <fdr@fdr.hu>
2024-05-01 21:34:43 +0200

    Additional date pattern tests

--- FDR.Tools.Library.Test/RenamePatternTest.cs
+++ FDR.Tools.Library.Test/RenamePatternTest.cs
@@ -42,6 +42,7 @@ namespace FDR.Tools.Library.Test
         [TestCase("{NAME:0,2:order}", "name", "0,2", "order")]
         [TestCase("abc{NAME:0,2:order}def", "name", "0,2", "order")]
         [TestCase("{EDATE:yyMMdd}", "edate", "yyMMdd")]
+        [TestCase("{EDATE:yyMMdd_HHmmss}", "edate", "yyMMdd_HHmmss")]
         [TestCase("abc{EDATE:yyMMdd}def", "edate", "yyMMdd")]
         [TestCase("{COUNTER}", "counter")]
         [TestCase("{COUNTER:2}", "counter", "2")]
@@ -80,16 +81,26 @@ namespace FDR.Tools.Library.Test
 
             Rename.EvaluateNamePattern("pre_{cdate:yyyy}_{cdate:MM}_{cdate:dd}_post", file)
                 .Should().Be($"pre_2007_08_09_post");
+            Rename.EvaluateNamePattern("pre_{cdate:yyyy_MM_dd}_post", file)
+                .Should().Be($"pre_2007_08_09_post");
             Rename.EvaluateNamePattern("pre_{cdate:yyyy}_{cdate:MM}_{cdate:dd}_post", folder)
+                .Should().Be($"pre_2001_02_03_post");
+            Rename.EvaluateNamePattern("pre_{cdate:yyyy_MM_dd}_post", folder)
                 .Should().Be($"pre_2001_02_03_post");
 
             Rename.EvaluateNamePattern("pre_{mdate:yyyy}_{mdate:MM}_{mdate:dd}_post", file)
                 .Should().Be($"pre_2010_11_12_post");
+            Rename.EvaluateNamePattern("pre_{mdate:yyyy_MM_dd}_post", file)
+                .Should().Be($"pre_2010_11_12_post");
             Rename.EvaluateNamePattern("pre_{mdate:yyyy}_{mdate:MM}_{mdate:dd}_post", folder)
+                .Should().Be($"pre_2004_05_06_post");
+            Rename.EvaluateNamePattern("pre_{mdate:yyyy_MM_dd}_post", folder)
                 .Should().Be($"pre_2004_05_06_post");
 
             var date = DateTime.Now;
             Rename.EvaluateNamePattern("pre_{now:yyyy}_{now:MM}_{now:dd}_post", null)
+                .Should().Be($"pre_{date:yyyy}_{date:MM}_{date:dd}_post");
+            Rename.EvaluateNamePattern("pre_{now:yyyy_MM_dd}_post", null)
                 .Should().Be($"pre_{date:yyyy}_{date:MM}_{date:dd}_post");
         }
 
@@ -101,12 +112,18 @@ namespace FDR.Tools.Library.Test
 
             Rename.EvaluateFolderNamePattern("pre_{cdate:yyyy}_{cdate:MM}_{cdate:dd}_post", folder)
                 .Should().Be($"pre_2001_02_03_post");
+            Rename.EvaluateFolderNamePattern("pre_{cdate:yyyy_MM_dd}_post", folder)
+                .Should().Be($"pre_2001_02_03_post");
 
             Rename.EvaluateFolderNamePattern("pre_{mdate:yyyy}_{mdate:MM}_{mdate:dd}_post", folder)
+                .Should().Be($"pre_2004_05_06_post");
+            Rename.EvaluateFolderNamePattern("pre_{mdate:yyyy_MM_dd}_post", folder)
                 .Should().Be($"pre_2004_05_06_post");
 
             var date = DateTime.Now;
             Rename.EvaluateFolderNamePattern("pre_{now:yyyy}_{now:MM}_{now:dd}_post", null)
+                .Should().Be($"pre_{date:yyyy}_{date:MM}_{date:dd}_post");
+            Rename.EvaluateFolderNamePattern("pre_{now:yyyy_MM_dd}_post", null)
                 .Should().Be($"pre_{date:yyyy}_{date:MM}_{date:dd}_post");
 
             Rename.EvaluateFolderNamePattern("pre_{pfolder:0,2}_{pfolder:2,2}_post", folder)
@@ -121,12 +138,18 @@ namespace FDR.Tools.Library.Test
 
             Rename.EvaluateFileNamePattern("pre_{cdate:yyyy}_{cdate:MM}_{cdate:dd}_post", file, 1)
                 .Should().Be($"pre_2007_08_09_post");
+            Rename.EvaluateFileNamePattern("pre_{cdate:yyyy_MM_dd}_post", file, 1)
+                .Should().Be($"pre_2007_08_09_post");
 
             Rename.EvaluateFileNamePattern("pre_{mdate:yyyy}_{mdate:MM}_{mdate:dd}_post", file, 1)
+                .Should().Be($"pre_2010_11_12_post");
+            Rename.EvaluateFileNamePattern("pre_{mdate:yyyy_MM_dd}_post", file, 1)
                 .Should().Be($"pre_2010_11_12_post");
 
             var date = DateTime.Now;
             Rename.EvaluateFileNamePattern("pre_{now:yyyy}_{now:MM}_{now:dd}_post", (FileInfo)null, 1)
+                .Should().Be($"pre_{date:yyyy}_{date:MM}_{date:dd}_post");
+            Rename.EvaluateFileNamePattern("pre_{now:yyyy_MM_dd}_post", (FileInfo)null, 1)
                 .Should().Be($"pre_{date:yyyy}_{date:MM}_{date:dd}_post");
 
             Rename.EvaluateFileNamePattern("pre_{pfolder:0,2}_{pfolder:2,2}_post", file, 1)
@@ -144,12 +167,18 @@ namespace FDR.Tools.Library.Test
 
             Rename.EvaluateFileNamePattern("pre_{cdate:yyyy}_{cdate:MM}_{cdate:dd}_post", exifFile, 1)
                 .Should().Be($"pre_2007_08_09_post");
+            Rename.EvaluateFileNamePattern("pre_{cdate:yyyy_MM_dd}_post", exifFile, 1)
+                .Should().Be($"pre_2007_08_09_post");
 
             Rename.EvaluateFileNamePattern("pre_{mdate:yyyy}_{mdate:MM}_{mdate:dd}_post", exifFile, 1)
+                .Should().Be($"pre_2010_11_12_post");
+            Rename.EvaluateFileNamePattern("pre_{mdate:yyyy_MM_dd}_post", exifFile, 1)
                 .Should().Be($"pre_2010_11_12_post");
 
             var date = DateTime.Now;
             Rename.EvaluateFileNamePattern("pre_{now:yyyy}_{now:MM}_{now:dd}_post", (ExifFile)null, 1)
+                .Should().Be($"pre_{date:yyyy}_{date:MM}_{date:dd}_post");
+            Rename.EvaluateFileNamePattern("pre_{now:yyyy_MM_dd}_post", (ExifFile)null, 1)
                 .Should().Be($"pre_{date:yyyy}_{date:MM}_{date:dd}_post");
 
             Rename.EvaluateFileNamePattern("pre_{pfolder:0,2}_{pfolder:2,2}_post", exifFile, 1)
