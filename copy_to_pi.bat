6230ec35dd13dbd1c947b3a8f63d7c6af73d254c
FDR <fdr@fdr.hu>
2024-07-01 15:49:24 +0200

    nuget updates

--- copy_to_pi.bat
+++ copy_to_pi.bat
@@ -1,4 +1,4 @@
 @echo off
-pscp -r ./FDR/bin/Release/net6.0/* pi@192.168.1.3:/home/pi/FDR/
-pscp -r ./FDR.UI/bin/Release/net6.0/* pi@192.168.1.3:/home/pi/FDR.UI/
+pscp -r ./FDR/bin/Release/net8.0/* pi@192.168.1.3:/home/pi/FDR/
+pscp -r ./FDR.Web/bin/Release/net8.0/* pi@192.168.1.3:/home/pi/FDR.Web/
 pause
